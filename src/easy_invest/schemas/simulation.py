76bb5f69425e45bae0f0cc5d2e62ba3af698995e
Teofilo0 <teofilocalixto97@gmail.com>
2025-09-16 15:43:04 -0300

    .

--- src/easy_invest/schemas/simulation.py
+++ src/easy_invest/schemas/simulation.py
@@ -1,11 +1,11 @@
 from pydantic import BaseModel
-from typing import Optional, Dict, Any
+from typing import Optional
 from datetime import datetime
 
 class Simulation(BaseModel):
-    id: int
+    id: Optional[int]
     user_id: int
-    type: str
-    input_data: Optional[Dict[str, Any]]
-    result_data: Optional[Dict[str, Any]]
-    created_at: datetime
+    type: str 
+    input_data: dict
+    result_data: dict
+    created_at: datetime = datetime.now()
