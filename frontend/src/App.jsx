d208616d366a075ca078106de2e73752b35fcf0e
Ayslan Francisco Monteiro Neto <20221101110051@ifrn.local>
2025-10-30 14:01:27 -0300

    style: Iniciando tela de register

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
 import Login from './components/Login'
+import Register from './components/Register'
 function App() {
 
   return (
     <>
-      <Login></Login>
+      <Register></Register>
     </>
   )
 }
